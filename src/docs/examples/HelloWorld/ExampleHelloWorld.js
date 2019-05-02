3d22c3c9122d85963ef3b70383ec1f2350031849
Mariann M <mariann19@gmail.com>
2019-05-02 18:09:34 -0400

    documentation

---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import HelloWorld from "../../../components/HelloWorld/HelloWorld";
+import HelloWorld from "ps-react/HelloWorld";
+
+/** Custom message */
+export default function ExampleHelloWorld() {
+  return <HelloWorld message="Reuseable React Components" />;
+}
