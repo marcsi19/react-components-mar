c8e8705e698bb2becfb378a6f8db799301616004
Mariann M <mariann19@gmail.com>
2019-05-03 16:56:34 -0400

    TravisCI added

--- src/components/ProgressBar/ProgressBar.spec.js
+++ src/components/ProgressBar/ProgressBar.spec.js
@@ -1,8 +1,6 @@
 import React from "react";
-
 import { shallow, configure } from "enzyme";
 import ProgressBar from "./ProgressBar";
-
 import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
