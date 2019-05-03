1d85990f12d37a91baaf6a8dbc00e9fe174da216
Mariann M <mariann19@gmail.com>
2019-05-03 14:46:46 -0400

    styling

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -404,6 +404,9 @@ module.exports = function(webpackEnv) {
               use: getStyleLoaders({
                 importLoaders: 1,
                 sourceMap: isEnvProduction && shouldUseSourceMap
+                // NEXT TWO LINES for CSS MODULES!!!!
+                // modules: true,
+                // localIdentName: `[name]_[local]_[hash:base64:5]`
               }),
               // Don't consider CSS imports dead code even if the
               // containing package claims to have no side effects.
