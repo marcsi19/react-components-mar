4596f5c1f28bb89305f58db432e5144587434cc0
Mariann M <mariann19@gmail.com>
2019-05-04 13:23:08 -0400

    Updates

---
+++ precache-manifest.b8fdcb44a065b843b72ae253b9c3f865.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7ec7edafd0b375596fd9122c39c148d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f17810eec957e5a11996",
+    "url": "/static/css/2.52738377.chunk.css"
+  },
+  {
+    "revision": "2ad912ee6d8cd5543e38",
+    "url": "/static/css/main.ea8938b9.chunk.css"
+  },
+  {
+    "revision": "f17810eec957e5a11996",
+    "url": "/static/js/2.c3cb80e6.chunk.js"
+  },
+  {
+    "revision": "2ad912ee6d8cd5543e38",
+    "url": "/static/js/main.a015c234.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
